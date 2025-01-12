5fe192ca8ca36c1803508baf879042ae1f67cb3b
RealMrLBronzey9 <117611460+RealMrLBronzey9@users.noreply.github.com>
2025-01-12 17:07:43 -0600

    Changed Comments

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ while isRunning:
     # Call macro based on switch entered
     match inputInt:
         case 1:
-            # Discord ~☆
+            # Mainly for my discord, type "~☆" and send message
             keyboard.type(" ~☆")
             keyboard.tap(Key.enter)
 
@@ -27,30 +27,37 @@ while isRunning:
             os.startfile("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe")
 
         case 3:
-            # Via Geforce Experience, save an instant replay from the last 
+            # Via Geforce Experience, save an instant replay
             keyboard.press(Key.alt)
             keyboard.tap(Key.f10)
             keyboard.release(Key.alt)
             
         case 4:
+            # Under construction
             print(4)
         
         case 5:
+            # Under construction
             print(5)  
 
         case 6:
+            # Under construction
             print(6)
 
         case 7:
+            # Under construction
             print(7)
 
         case 8:
+            # Under construction
             print(8)
 
         case 9:
+            # Under construction
             print(9)
 
         case 10:
+            # Under construction
             print(10)
         
 
